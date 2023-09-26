59572c1de8863611368c06cd3ec810baca53a7cd
Abraomukas <abraomukas@gmail.com>
2023-09-26 13:53:38 +0400

    feat: Modify project structure

---
+++ scrapper.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup
+import matplotlib.pyplot as plt
+import numpy as np
+from datetime import datetime, timedelta
+
+TIMESTAMP_FORMAT = "%b %d, %I:%M:%S %p UTC%z"
+URL = "https://www.google.com/finance/quote/ACN:NYSE?hl=en"
+
+def extract_values_from_url():
+    try:
+        response = requests.get(URL)
+
+        if response.status_code == 200:
+            # Calculate the offset (NY UTC = -4)
+            utc_offset = timedelta(hours=-4)
+
+            # Save the time when the request was made
+            timestamp = (datetime.utcnow() + utc_offset).strftime(TIMESTAMP_FORMAT)
+
+            # Parse the HTML content of the page
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            # Find the specific <div> element by its class
+            stock_prize_div = soup.find("div", class_="kf1m0")
+
+            if stock_prize_div:
+                return timestamp,   stock_prize_div.text.strip()
+            else:
+                print(f"Div with class 'kf1m0' not found on the page.")
+        else:
+            print(
+                f"Failed to retrieve content from {URL}. Status code: {response.status_code}"
+            )
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+def populate_values():
+    with open("output.txt", "a") as file:
+        value = extract_values_from_url(URL)
+
+        file.write(value.replace("$", "") + "\n")
+
+
+if __name__ == "__main__":
+    time, value = extract_values_from_url()
+
+    print("Timestamp - " + time)
+    print("Stock value - " + value)
+
+    timeAxis = ["10am", "12pm", "2pm", "4pm"]
+    valueAxis = np.array([3, 8, float(value.replace("$", "")), 10])
+
+    plt.plot(timeAxis, valueAxis, marker="o", linestyle="dashed")
+
+    plt.xlabel("TIME")
+    plt.ylabel("VALUE")
+    plt.title("Accenture's stock")
+
+    plt.show()
