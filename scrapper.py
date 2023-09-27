aa277c577a4bf08c4adda4f3724276cc18b90586
Abraomukas <abraomukas@gmail.com>
2023-09-27 15:27:16 +0400

    feat: Add file dumping

--- scrapper.py
+++ scrapper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import datetime, timedelta
+import time
 
 TIMESTAMP_FORMAT = "%b %d, %I:%M:%S %p UTC%z"
 URL = "https://www.google.com/finance/quote/ACN:NYSE?hl=en"
@@ -25,7 +26,7 @@ def extract_values_from_url():
             stock_prize_div = soup.find("div", class_="kf1m0")
 
             if stock_prize_div:
-                return timestamp,   stock_prize_div.text.strip()
+                return timestamp, stock_prize_div.text.strip()
             else:
                 print(f"Div with class 'kf1m0' not found on the page.")
         else:
@@ -38,24 +39,38 @@ def extract_values_from_url():
 
 def populate_values():
     with open("output.txt", "a") as file:
-        value = extract_values_from_url(URL)
+        time, value = extract_values_from_url()
 
-        file.write(value.replace("$", "") + "\n")
+        input_date = datetime.strptime(time, "%b %d, %I:%M:%S %p UTC")
+
+        timestamp = input_date.strftime("%I:%M %p")
+
+        file.write(timestamp + " <> " + value.replace("$", "") + "\n")
+
+        print("Entry added!")
 
 
 if __name__ == "__main__":
-    time, value = extract_values_from_url()
+    # time, value = extract_values_from_url()
 
-    print("Timestamp - " + time)
-    print("Stock value - " + value)
+    # print("Timestamp - " + time)
+    # print("Stock value - " + value)
 
-    timeAxis = ["10am", "12pm", "2pm", "4pm"]
-    valueAxis = np.array([3, 8, float(value.replace("$", "")), 10])
+    # timeAxis = ["10am", "12pm", "2pm", "4pm"]
+    # valueAxis = np.array([3, 8, float(value.replace("$", "")), 10])
 
-    plt.plot(timeAxis, valueAxis, marker="o", linestyle="dashed")
+    # plt.plot(timeAxis, valueAxis, marker="o", linestyle="dashed")
 
-    plt.xlabel("TIME")
-    plt.ylabel("VALUE")
-    plt.title("Accenture's stock")
+    # plt.xlabel("TIME")
+    # plt.ylabel("VALUE")
+    # plt.title("Accenture's stock")
 
-    plt.show()
+    # plt.show()
+
+    ###
+
+    while True:
+        populate_values()
+
+        time.sleep(60)
+
